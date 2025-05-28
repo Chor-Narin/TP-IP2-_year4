73bed4a7b3a17635cfec88c8516837ae01c50a62
unknown <naritkh178@gmail.com>
2025-05-28 17:23:00 +0700

    TP10

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS with configuration
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
